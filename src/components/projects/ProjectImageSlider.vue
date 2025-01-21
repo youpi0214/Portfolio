a4b659c04334df0c7e97517c2b85713e57078a06
Yannick Andre Ouamba <yannick-andre.ouamba.1@ulaval.ca>
2025-01-21 03:18:40 -0500

    add full screen image on click

--- src/components/projects/ProjectImageSlider.vue
+++ src/components/projects/ProjectImageSlider.vue
@@ -9,7 +9,19 @@
           class="absolute inset-0 flex items-center justify-center transition-opacity duration-500"
           :class="{ 'opacity-0': currentSlide !== index, 'opacity-100': currentSlide === index }"
       >
-        <img :src="image" alt="Slide" class="object-contain w-full h-full" />
+        <img :src="image" alt="Slide" class="object-contain w-full h-full cursor-pointer" @click="showFullscreen"/>
+        <!-- Fullscreen Overlay -->
+        <div
+            v-if="isFullscreen"
+            class="fixed inset-0 z-50 bg-black bg-opacity-90 flex items-center justify-center cursor-pointer"
+            @click="closeFullscreen"
+        >
+          <img
+              :src="image"
+              alt="Full-size Image"
+              class="rounded max-h-[90%] max-w-[90%]"
+          />
+        </div>
       </div>
     </div>
 
@@ -32,27 +44,36 @@
         class="absolute top-0 start-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none"
         @click="prevSlide(images)"
     >
-      <IconPrevious />
+      <IconPrevious/>
     </button>
     <button
         type="button"
         class="absolute top-0 end-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none"
         @click="nextSlide(images)"
     >
-      <IconNext />
+      <IconNext/>
     </button>
   </div>
 </template>
 
 
 <script setup lang="ts">
-import { ref } from "vue";
+import {ref} from "vue";
 import IconPrevious from "@/components/icons/IconPrevious.vue";
 import IconNext from "@/components/icons/IconNext.vue";
 
 defineProps<{ images: string[] }>();
-
+const isFullscreen = ref(false);
 const currentSlide = ref<number>(0);
+
+const showFullscreen = () => {
+  isFullscreen.value = true;
+
+};
+const closeFullscreen = () => {
+  isFullscreen.value = false;
+
+};
 
 const goToSlide = (index: number) => {
   currentSlide.value = index;
