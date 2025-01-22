c8d082541876b0061ec0ead3c7e467682e51ac30
Yannick Andre Ouamba <yannick-andre.ouamba.1@ulaval.ca>
2025-01-22 12:08:20 -0500

    remove slider indicator

--- src/components/projects/ProjectImageSlider.vue
+++ src/components/projects/ProjectImageSlider.vue
@@ -31,19 +31,6 @@
       </div>
     </div>
 
-    <!-- Slider indicators -->
-    <div class="absolute z-30 flex -translate-x-1/2 bottom-5 left-1/2 space-x-3 rtl:space-x-reverse">
-      <button
-          v-for="(_, index) in mediaContent"
-          :key="index"
-          type="button"
-          class="w-3 h-3 rounded-full bg-primary"
-          :class="{ 'bg-accent': currentSlide === index }"
-          @click="goToSlide(index)"
-          :aria-label="'Slide ' + (index + 1)"
-      ></button>
-    </div>
-
     <!-- Slider controls -->
     <button
         type="button"
